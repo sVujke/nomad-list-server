b4cf7bc47ee6424770e58d4b4f38e9b8f651aaa9
dkeske <danielkeske@hotmail.com>
2017-02-07 23:00:25 +0100

    Pokusaj standardizacije podataka
    Dodata jedna nova ruta sa podacima

--- server.py
+++ server.py
@@ -2,7 +2,8 @@ import web
 import json
 
 urls = (
-    '/', 'index'
+    '/', 'index',
+    '/2', 'index2'
 )
 
 class index:
@@ -12,6 +13,12 @@ class index:
         dzejson = open('data.json')
         return dzejson.read()
         # return "Hello world"
+class index2:
+    def GET(self):
+        web.header('Content-Type', 'application/json')
+        web.header('Access-Control-Allow-Origin', '*')
+        dzejson = open('data1.json')
+        return dzejson.read()
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
